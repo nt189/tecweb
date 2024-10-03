3b4c421467021a3b08d69b1544c446f1853cea67
nt189 <antonio.moralesba@alumno.buap.mx>
2024-10-03 01:50:48 -0600

    p08 avances

--- tecweb/practicas/p08/set_producto_v2.php
+++ tecweb/practicas/p08/set_producto_v2.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <?php
 $nombre = $_POST['nombre'];
 $marca  = $_POST['marca'];
@@ -18,20 +21,93 @@ if ($link->connect_errno)
 }
 
 // Validar que el nombre, modelo y marca no existan ya en la BD
-$sql_validacion = "SELECT * FROM productos WHERE nombre = '$nombre' AND marca = '$marca' AND modelo = '$modelo'";
-$stmt = $link->query($sql_validacion);
-echo $resultado;
+if ( $sqlval = $link->query("SELECT * FROM productos") ) 
+		{
+            /** Se extraen las tuplas obtenidas de la consulta */
+			$row = $sqlval->fetch_all(MYSQLI_ASSOC);
+
+            /** Se crea un arreglo con la estructura deseada */
+            foreach($row as $num => $registro) {            // Se recorren tuplas
+                foreach($registro as $key => $value) {      // Se recorren campos
+                    $data[$num][$key] = ($value);
+                }
+            }
+
+			/** útil para liberar memoria asociada a un resultado con demasiada información */
+			$sqlval->free();
+		}
+
+$val = false;
+foreach($data as $dat){
+    if($dat['nombre'] == $nombre && $dat['modelo'] == $modelo && $dat['marca'] == $marca){
+        $val = true;
+    }
+}
+
+// echo 'resultado de val:'.$val;
 
 /** Crear una tabla que no devuelve un conjunto de resultados */
-// $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-// if ( $link->query($sql) ) 
-// {
-//     echo 'Producto insertado con ID: '.$link->insert_id;
-// }
-// else
-// {
-// 	echo 'El Producto no pudo ser insertado =(';
-// }
+$sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, 'img/{$imagen}')";
+if (!$val){
+    if( $link->query($sql) ){
+        echo 'Producto insertado con ID: '.$link->insert_id;
+        $idi=$link->insert_id;
+    }
+    else{
+        echo 'El Producto no pudo ser insertado =(';
+    }
+}
+else{
+	echo 'El Producto no pudo ser insertado =(, se esta repitiendo el producto: Nombre:'.$nombre.'. Marca:'.$marca.'. Modelo:'.$modelo.'.';
+}
 
 $link->close();
 ?>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<title>Producto</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+	<body>
+		<h3>PRODUCTO</h3>
+
+		<br/>
+		
+		<?php if( isset($row) && !$val) : ?>
+
+			<table class="table">
+				<thead class="thead-dark">
+					<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Marca</th>
+					<th scope="col">Modelo</th>
+					<th scope="col">Precio</th>
+					<th scope="col">Unidades</th>
+					<th scope="col">Detalles</th>
+					<th scope="col">Imagen</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row"><?= $idi ?></th>
+						<td><?= $nombre ?></td>
+						<td><?= $marca ?></td>
+						<td><?= $modelo ?></td>
+						<td><?= $precio ?></td>
+						<td><?= $unidades ?></td>
+						<td><?= $detalles ?></td>
+						<td><img src=<?= $imagen ?> width="150" height="150"></td>
+					</tr>
+				</tbody>
+			</table>
+
+		<?php elseif(!empty($id)) : ?>
+
+			 <script>
+                alert('El ID del producto no existe');
+             </script>
+
+		<?php endif; ?>
+	</body>
+</html>
