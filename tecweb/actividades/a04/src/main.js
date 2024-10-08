e1d4acbf9c4b08f4993b8faa820a9a853b49bd53
nt189 <antonio.moralesba@alumno.buap.mx>
2024-10-07 23:31:09 -0600

    a04: ejercicios 2-3 funcionando

--- tecweb/actividades/a04/src/main.js
+++ tecweb/actividades/a04/src/main.js
@@ -2,3 +2,31 @@ function ejemplo1() {
     ddocument.write('Hola Mundo');
 }
 
+function ejemplo2() {
+    var nombre = 'Juan';
+    var edad = 10;
+    var altura = 1.92;
+    var casado = false;
+
+    document.write(nombre);
+    document.write('<br>');
+    document.write(edad);
+    document.write('<br>');
+    document.write(altura);
+    document.write('<br>');
+    document.write(casado);
+}
+
+function ejemplo3() {
+    var nombre;
+    var edad;
+
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
