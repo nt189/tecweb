6cd40c7a4538fb393b4e8fdada23df2aa3a454b9
nt189 <antonio.moralesba@alumno.buap.mx>
2024-10-18 17:06:41 -0600

    p10: Corección

--- tecweb/practicas/p10/product_app/backend/create.php
+++ tecweb/practicas/p10/product_app/backend/create.php
@@ -6,44 +6,40 @@
     if(!empty($producto)) {
         // SE TRANSFORMA EL STRING DEL JASON A OBJETO
         $jsonOBJ = json_decode($producto);
-        /**
-         * SUSTITUYE LA SIGUIENTE LÍNEA POR EL CÓDIGO QUE REALICE
-         * LA INSERCIÓN A LA BASE DE DATOS. COMO RESPUESTA REGRESA
-         * UN MENSAJE DE ÉXITO O DE ERROR, SEGÚN SEA EL CASO.
-         */
+        
+        // Extrae los datos del producto
+        $nombre = $jsonOBJ->nombre;
+        $marca = $jsonOBJ->marca;
+        $modelo = $jsonOBJ->modelo;
+        $precio = $jsonOBJ->precio;
+        $detalles = $jsonOBJ->detalles;
+        $unidades = $jsonOBJ->unidades;
+        $imagen = $jsonOBJ->imagen;
+
+        // Verifica si el producto ya existe
+        $sql = "SELECT * FROM productos WHERE nombre = ? AND eliminado = 0";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bind_param("s", $nombre);
+        $stmt->execute();
+        $resultados = $stmt->get_result();
+
+        if ($resultados->num_rows > 0) {
+            echo 'El Producto ya existe';
+            $stmt->close();
+            $conexion->close();
+            exit;
+        }
+
+        $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
+        if ( $conexion->query($sql) ) 
+        {
+            echo 'Producto insertado con ID: '.$conexion->insert_id;
+        }
+        else
+        {
+            echo 'El Producto no pudo ser insertado =(';
+        }
+
         // echo '[SERVIDOR] Nombre: '.$jsonOBJ->nombre;
-    }
-
-    // Extrae los datos del producto
-    $nombre = $jsonOBJ->nombre;
-    $marca = $jsonOBJ->marca;
-    $modelo = $jsonOBJ->modelo;
-    $precio = $jsonOBJ->precio;
-    $detalles = $jsonOBJ->detalles;
-    $unidades = $jsonOBJ->unidades;
-    $imagen = $jsonOBJ->imagen;
-
-    // Verifica si el producto ya existe
-    $sql = "SELECT * FROM productos WHERE nombre = ? AND eliminado = 0";
-    $stmt = $conexion->prepare($sql);
-    $stmt->bind_param("s", $nombre);
-    $stmt->execute();
-    $resultados = $stmt->get_result();
-
-    if ($resultados->num_rows > 0) {
-        echo 'El Producto ya existe';
-        $stmt->close();
-        $conexion->close();
-        exit;
-    }
-
-    $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', 0)";
-    if ( $conexion->query($sql) ) 
-    {
-        echo 'Producto insertado con ID: '.$conexion->insert_id;
-    }
-    else
-    {
-        echo 'El Producto no pudo ser insertado =(';
     }
 ?>
