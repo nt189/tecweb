3a014b9111184978e2f15d7f547a1a0ee66d098e
nt189 <antonio.moralesba@alumno.buap.mx>
2024-11-06 16:36:24 -0600

    Act7: carga de archivos y creacion de clase base de datos

---
+++ tecweb/actividades/a07/product_app/backend/myapi/DataBase.php
@@ -0,0 +1,21 @@
+<?php
+class DataBase {
+    protected $conexion;
+
+    public function __construct($user, $pass, $db) {
+        $this->conexion = @mysqli_connect(
+            'localhost',
+            $user,
+            $pass,
+            $db
+        );
+    
+        // NOTA: si la conexión falló, $this->conexion contendrá false
+        if (!$this->conexion) {
+            echo ('¡Base de datos NO conectada!');
+        }
+    }
+}
+// pruebas de conexion
+// $dat = new DataBase('root', 'zP*liGgdxEbBXjyk', 'marketzone');
+?>
