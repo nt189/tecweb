74ef7b37a15c6d822753eabd022f2abd39f927a0
nt189 <antonio.moralesba@alumno.buap.mx>
2024-11-07 13:33:33 -0600

    a07: cambios en base de datos e inicio de products.php

--- tecweb/actividades/a07/product_app/backend/myapi/DataBase.php
+++ tecweb/actividades/a07/product_app/backend/myapi/DataBase.php
@@ -1,21 +1,23 @@
 <?php
-class DataBase {
-    protected $conexion;
+    namespace TECWEB\MYAPI;
 
-    public function __construct($user, $pass, $db) {
-        $this->conexion = @mysqli_connect(
-            'localhost',
-            $user,
-            $pass,
-            $db
-        );
-    
-        // NOTA: si la conexión falló, $this->conexion contendrá false
-        if (!$this->conexion) {
-            echo ('¡Base de datos NO conectada!');
+    abstract class DataBase {
+        protected $conexion;
+
+        public function __construct($user, $pass, $db) {
+            $this->conexion = @mysqli_connect(
+                'localhost',
+                $user,
+                $pass,
+                $db
+            );
+        
+            // NOTA: si la conexión falló, $this->conexion contendrá false
+            if (!$this->conexion) {
+                echo ('¡Base de datos NO conectada!');
+            }
         }
     }
-}
-// pruebas de conexion
-// $dat = new DataBase('root', 'zP*liGgdxEbBXjyk', 'marketzone');
+    // pruebas de conexion
+    // $dat = new DataBase('root', 'zP*liGgdxEbBXjyk', 'marketzone');
 ?>
--- tecweb/actividades/a07/product_app/backend/myapi/Products.php
+++ tecweb/actividades/a07/product_app/backend/myapi/Products.php
@@ -1,5 +1,49 @@
 <?php
-    class Products{
+    namespace TECWEB\MYAPI;
+
+    use TECWEB\MYAPI\DataBase;
+    require_once __DIR__ . '/DataBase.php';
+
+    class Products extends DataBase{
+        private $data;
+
+
+        public function __construct($db , $user = 'root', $pass = 'zP*liGgdxEbBXjyk'){
+            $this->data = array();
+            parent::__construct($user, $pass, $db);
+        }
+
+        public function add(){
+
+        }
+
+        public function delete(){
+
+        }
+
+        public function edit(){
+
+        }
+
+        public function list(){
+
+        }
+
+        public function search(){
+
+        }
+
+        public function single(){
+
+        }
+
+        public function singleByName(){
+
+        }
+
+        public function getData(){
+
+        }
 
     }
 ?>
